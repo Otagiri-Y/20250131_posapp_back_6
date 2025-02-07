50c65e0cb1d0588a2e4bee67b835610599b7dd6b
Otagiri-Y <otagiri.yo.hey@gmail.com>
2025-02-08 08:59:16 +0900

    gunicorn

--- gunicorn.py
+++ gunicorn.py
@@ -1,3 +1,4 @@
+"""
 import multiprocessing
 import os
 
@@ -7,3 +8,19 @@ bind = "0.0.0.0:8080"
 
 worker_class = "uvicorn.workers.UvicornWorker"
 workers = multiprocessing.cpu_count () * 2 + 1
+"""
+import multiprocessing
+import os
+
+name = "Gunicorn config for FastAPI"
+
+# 環境変数 PORT を取得
+port = os.getenv('PORT', '8080')
+
+# 確認用に出力
+print(f"Using PORT: {port}")
+
+# バインド設定
+bind = f"0.0.0.0:{port}"
+worker_class = "uvicorn.workers.UvicornWorker"
+workers = multiprocessing.cpu_count() * 2 + 1
